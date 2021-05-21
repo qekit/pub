adfda3aee03f56684d9043f2142729646db919f1
lj-v-18 <474130428@qq.com>
2021-05-21 11:12:08 +0800

    1.PC2连接rosmaster是PC1上的，所以环境变量.bashrc是一定要载入的，因为其中配置了ros双机通信 2.因为配置在环境变量中的是hostname，导致载入了.bashrc也没用，所以要替换为IP地址，但是我不想改文件了，就直接在pc2里面export了 3.真心不容易，我已经快心碎了，艾，三天了，所以该求救还得求救阿

--- pc2_autostart.sh
+++ pc2_autostart.sh
@@ -5,7 +5,7 @@
 
 ##### 配置 begin ####
 #脚本执行日志文件
-LOG_FILE=/home/${USER}/autostart_err.log
+LOG_FILE=/home/${USER}/autostart.log
 #ROS环境变量路径
 ROS_PACKAGES_PATH=/opt/ros/melodic/setup.bash
 #无人矿卡感知系统启动脚本run_perception.sh存放路径
@@ -15,36 +15,41 @@ SCRIPT_PATH=/home/${USER}/catkin_ws/shell/
 MASTER_COUNT=20
 #两次尝试间隔
 MASTER_WAIT=10
-TMP_FILE=/home/${USER}/.tmp.swap
+#TMP_FILE=/home/${USER}/.tmp.swap
 ##### 配置 end ####
 
 index=1
 #rm -f $TMP_FILE
 #touch $TMP_FILE
+
+#下面是PC2为了能够连接PC1上的rosmaster必须载入的环境变量
+#其实可以将～/.bashrc中pc1和pc2换成IP地址，这样就不用export两句了，但是我不想改了
+source /etc/profile
 source /opt/ros/melodic/setup.bash
+source /home/neousys/.bashrc
+export ROS_HOSTNAME=192.168.0.13
+export ROS_MASTER_URI=http://192.168.0.11:11311
 
 while [ $index -le $MASTER_COUNT ]
      do
-           gnome-terminal -- bash -c "source /opt/ros/melodic/setup.bash;\
-               echo "$(rosnode list)" >>$TMP_FILE"
-      #      roscore_stat=$(tail -n 1 $TMP_FILE)
-      #     error_code=${roscore_stat:0:1}
-      #     echo "$error_code"
-      #     flag="/"
-      #     if [[ $error_code = $flag ]];then
-      #          d1=`date +%Y%m%d-%H:%M:%S`
-      #           echo "$d1 roscore ok" >>$LOG_FILE
-      #           cd $SCRIPT_PATH
-      #          ./run_perception.sh 
-      #           let index=1000
+           roscore_stat=$(rosnode list)
+           error_code=${roscore_stat:0:1}
+           echo "$error_code"
+           flag="/"
+           if [[ $error_code = $flag ]];then
+                d1=`date +%Y%m%d-%H:%M:%S`
+                 echo "$d1 roscore ok" >>$LOG_FILE
+                 cd $SCRIPT_PATH
+                ./run_perception.sh
+                 let index=1000
       #           rm -f $TMP_FILE
-      #           exit
-      #     else 
-      #           d1=`date +%Y%m%d-%H:%M:%S`
-      #           echo "$d1 cannot connect roscore" >>$LOG_FILE
-      #           let index++
-      #          sleep $MASTER_WAIT
-      #     fi
+                 exit
+           else
+                 d1=`date +%Y%m%d-%H:%M:%S`
+                 echo "$d1 cannot connect roscore" >>$LOG_FILE
+                 let index++
+                sleep $MASTER_WAIT
+           fi
 done
 echo "$d1 try to  connect roscore more than 20 times, exit" >>$LOG_FILE
-exit 
+exit
